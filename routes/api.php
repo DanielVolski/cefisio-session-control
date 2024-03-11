4db2bb5a4a41538dce5cbe38df7d5212aebfeeb5
Daniel Volski <danfvdaum3@hotmail.com>
2024-03-11 10:38:14 -0300

    creating ApprenticeController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApprenticeController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\SupervisorController;
 use Illuminate\Support\Facades\Route;
@@ -29,4 +30,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/supervisor/getAll', [SupervisorController::class, 'getAll']);
     Route::get('/supervisor/get/{id}', [SupervisorController::class, 'get']);
 })->name('supervisor');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/apprentice/create', [ApprenticeController::class, 'create']);
+    Route::delete('/apprentice/delete/{id}', [ApprenticeController::class, 'delete']);
+    Route::put('/apprentice/update/{id}', [ApprenticeController::class, 'update']);
+    Route::get('/apprenctice/getAll', [ApprenticeController::class, 'getAll']);
+    Route::get('/apprentice/get/{id}', [ApprenticeController::class, 'get']);
+})->name('apprentice');
 
